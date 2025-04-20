77dcf617ae765ba3c8d21272c78ae7fd786a0896
joca2511 <143613939+joca2511@users.noreply.github.com>
2025-04-20 22:42:14 +0000

    Mensagens entre cliente e servidor agora possuem estrutura

--- cliente.py
+++ cliente.py
@@ -4,17 +4,24 @@ ctx = zmq.Context()
 clientReq= ctx.socket(zmq.REQ)
 clientPull= ctx.socket(zmq.PULL)
 clientPush = ctx.socket(zmq.PUSH)
+clientEnd = "tcp://*:5600" ##endereco do cliente
 clientReq.connect("tcp://localhost:5555") ##endereco do Rep do load balancer (por agora serv)
-clientPull.bind("tcp://*:5600") ##endereco do pull do cliente
+clientPull.bind(clientEnd) ##endereco do pull do cliente
 clientPush.connect("tcp://localhost:5556") #endereco do Pull do load balancer (por agora serv)
+#estrutura da mensagem (ip de quem mandou,horarioLocal,tipo,conteudo)
+horarioLocal = 1
 while True:
     esc = int(input("Push(0) ou RepReq (1)? "))
+    mensagem = input("Mensagem a ser mandada: ")
     if esc == 0:
-        clientPush.send_string(f"batata")
+       
+        clientPush.send_string(f"{clientEnd},{horarioLocal},post,{mensagem}")
     elif esc == 1:
-        clientReq.send_string(f"caraca!")
+        
+        clientReq.send_string(f"{clientEnd},{horarioLocal},req,{mensagem}")
         recv = clientReq.recv_string()
         print(recv)
+    horarioLocal +=1
 
 
 
--- servidor.py
+++ servidor.py
@@ -3,30 +3,48 @@ import msgpack
 import time
 ctx = zmq.Context()
 poller = zmq.Poller()
+servEnd = "tcp://*:5556"
 servRep = ctx.socket(zmq.REP)
 servPull= ctx.socket(zmq.PULL)
 servPush = ctx.socket(zmq.PUSH)
 servRep.bind("tcp://*:5555")
-servPull.bind("tcp://*:5556")
+servPull.bind(servEnd)
 poller.register(servRep,zmq.POLLIN)
 poller.register(servPull,zmq.POLLIN)
 
-clock = 0
-##ctx = zmq.Context.instance()
-##pub.connect("tcp://localhost:5555")
-##Pub = 
-##Sub
+horarioLocal = 0
+class mensagem:
+    def __init__(self,recv):
+        conteudo = recv.split(",")
+        self.end = conteudo[0]
+        self.horarioRecv = int(conteudo[1])
+        self.tipoRecv = conteudo[2]
+        self.conteudoRecv = conteudo[3] 
+        
+
+def conferirHorario(horarioRecv):
+    global horarioLocal
+    if horarioRecv > horarioLocal:
+        horarioLocal = horarioRecv
+        print("Horario corrigido!")
+            
 while True:
     time.sleep(1)
     portas = dict(poller.poll())
+    
     if portas.get(servPull) == zmq.POLLIN:
         
         recv = servPull.recv_string()
-        print(recv)
+        msg = mensagem(recv)
+        conferirHorario(msg.horarioRecv)
+        print(msg.conteudoRecv)
     if portas.get(servRep) == zmq.POLLIN:
         recv = servRep.recv_string()
-        print(recv)
-        resp = recv + "2"
-        servRep.send_string(resp)
+        msg = mensagem(recv)
+        conferirHorario(msg.horarioRecv)
+        print(msg.conteudoRecv)
+        resp = msg.conteudoRecv + "2"
+        servRep.send_string(f"{servEnd},{horarioLocal},rep,{resp}")
+    
         
     
